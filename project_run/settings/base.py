2e93128f28de3d0bafc7657f17311038f7412886
day 1 - 1 <favorit1997@gmail.com>
2025-06-14 16:34:31 +0700

    add view company-details, url 'api/company_details/'

--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -114,3 +114,8 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+COMPANY_NAME = 'Бегуны до 18'
+SLOGAN = 'Молодые бегуны, обгонят всех!'
+CONTACTS = 'Город Москва, улица Ленина, дом 2'
