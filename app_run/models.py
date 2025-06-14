1f539a42954422f0e581bc68ccaba383ede49674
day 1 - 1 <favorit1997@gmail.com>
2025-06-14 17:56:43 +0700

    add model Run, add RunViewSet, create RunSerializers,

--- app_run/models.py
+++ app_run/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+
+class Run(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    athlete = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    def __str__(self):
+        return f'{self.athlete}'
--- app_run/views.py
+++ app_run/views.py
@@ -1,6 +1,10 @@
 from django.conf import settings
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from app_run.models import Run
+from app_run.serializers import RunSerializer
 
 
 @api_view(['GET'])
@@ -10,3 +14,9 @@ def company_details(request):
                'contacts': settings.CONTACTS
     }
     return Response(details)
+
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
+
