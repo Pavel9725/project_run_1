98f60bf52a1b27ff31702967fd73ce36bcbf9620
day 1 - 1 <favorit1997@gmail.com>
2025-06-16 22:22:08 +0700

    add Nested serializers and delete n+1

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -2,20 +2,24 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from app_run.models import Run
 
-
-class RunSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Run
-        fields = '__all__'
-
 class UserSerializers(serializers.ModelSerializer):
     type = serializers.SerializerMethodField() #type - вычисляемое поле
 
     class Meta:
         model = User
-        fields = ('id', 'date_joined', 'username', 'last_name', 'first_name', 'type')
+        fields = ('id', 'username', 'last_name', 'first_name', 'type')
 
     # Метод для вычисления поля type
 
     def get_type(self, obj):
         return 'coach' if obj.is_staff else 'athlete'
+
+class RunSerializer(serializers.ModelSerializer):
+    athlete_data = UserSerializers(read_only=True, source='athlete')
+    # client_name = serializers.CharField(source='client.company_name')
+    # email = serializers.CharField(source='client.user.email')
+
+    class Meta:
+        model = Run
+        fields = '__all__'
+
