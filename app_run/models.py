46b0a0090fdf7175b8e865c2565272d2637f3898
Pavel <favorit19977@gmail.com>
2025-07-08 20:44:44 +0700

    add positions

--- app_run/models.py
+++ app_run/models.py
@@ -36,5 +36,5 @@ class Challenge(models.Model):
 
 class Position(models.Model):
     run = models.ForeignKey(Run, on_delete=models.CASCADE, related_name='positions')
-    latitude = models.DecimalField(max_digits=7, decimal_places=4)
-    longitude = models.DecimalField(max_digits=8, decimal_places=4)
+    latitude = models.DecimalField(max_digits=7, decimal_places=4, default=0)
+    longitude = models.DecimalField(max_digits=8, decimal_places=4, default=0)
